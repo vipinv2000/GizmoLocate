bebd212234d110fbf0da14fe8807809ed532ced9
vipingost <vipinviswanath425@gmail.com>
2025-03-08 11:04:57 +0530

    All Work Completed

--- server/src/controller/shopController.js
+++ server/src/controller/shopController.js
@@ -134,7 +134,7 @@ export const login = async (req, res) => {
 };
 export const logout = async (req, res) => {
   try {
-    res.cookie('jwt', '', { maxAge: 0 });
+    res.cookie('shop_jwt', '', { maxAge: 0 });
     res.status(200).json({ message: 'Logged out successfully' });
   } catch (error) {
     console.log('Error in logout controller', error.message);
